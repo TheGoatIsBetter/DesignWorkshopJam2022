5623c2698f5cffb76b93b57a4ae9997e5c2958bf
JadynHax <jaonhax@gmail.com>
2022-10-16 02:46:15 -0700

    idfk

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -12,12 +12,12 @@ public class HealthBar : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        healthImage.fillAmount = health.currentHealth / health.maxHealth ;
+        healthImage.fillAmount = health.currentHealth / health.maxHealth;
     }
 }
